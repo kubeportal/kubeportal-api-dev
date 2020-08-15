46aef56b190ae6f44da3743aef4f3206b2d786d2
katharina.sachs <katharina.sachs@seerene.com>
2020-08-15 16:52:19 +0200

    check login credentials

--- api/app.py
+++ api/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from . import mock
+from . import security
+
+import json
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def hello_world():
     return jsonify({'Json sagt' : 'Hallo, I bims, der Json'})
+
 
 @app.route('/users/<uid>', methods=['GET'])
 def get_current_user(uid):
@@ -15,9 +20,11 @@ def get_current_user(uid):
     user = next((user for user in users if user['username'] == uid), {'user' : None})
     return jsonify(user)
 
+
 @app.route('/webapps', methods=['GET'])
 def get_all_webapps():
     return jsonify(mock.webapps)
+
 
 @app.route('/statistics/<metricname>', methods=['GET'])
 def get_cluster_statistics(metricname):
@@ -31,3 +38,12 @@ def get_cluster_statistics(metricname):
     return jsonify({'metric': None})
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    auth = json.loads(request.data.decode('utf-8'))
+    print(auth)
+    authenticated = security.authenticate(auth['username'], auth['password'])
+    if authenticated is None:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required"'})
+    else:
+        return ''
