890b0ae1af85077b30dabc02d3caf81d0de937d1
katharina.sachs <katharina.sachs@seerene.com>
2020-09-03 23:26:21 +0200

    remove flask login and refactor user-find-method

--- api/api.py
+++ api/api.py
@@ -1,10 +1,14 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from flask_login import login_required
+from functools import wraps
+import json
 
 from api import mock
 from api.consts import API_VERSION
-from api.login import User
+from api.login import User, find_user
+
 api_bp = Blueprint('api_bp', __name__)
+
 
 @api_bp.route('/')
 def hello_world():
@@ -12,21 +16,17 @@ def hello_world():
 
 
 @api_bp.route(API_VERSION + '/users/<uid>', methods=['GET'])
-@login_required
 def get_current_user(uid):
-    users = mock.users
-    user = User(next((user for user in users if user['username'] == uid), {'user': None}))
+    user = find_user(username=uid)
     return jsonify(user)
 
 
 @api_bp.route(API_VERSION + '/webapps', methods=['GET'])
-@login_required
 def get_all_webapps():
     return jsonify(mock.webapps)
 
 
 @api_bp.route(API_VERSION + '/statistics/<metricname>', methods=['GET'])
-@login_required
 def get_cluster_statistics(metricname):
     metrics = mock.metrics
     '''
@@ -36,3 +36,5 @@ def get_cluster_statistics(metricname):
         if next(iter(metric)).lower().replace(' ', '') == metricname:
             return jsonify(metric)
     return jsonify({'metric': None})
+
+
